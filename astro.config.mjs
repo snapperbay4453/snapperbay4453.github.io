9437d7ed3146eb94bcf37758a2c69c8ddf422f41
Kyeonghwan Son <snapperbay4453@gmail.com>
2023-08-30 06:56:07 +0900

    apply Unplugin Icons with Iconify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
+import Icons from 'unplugin-icons/vite';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,12 @@ export default defineConfig({
       status: 302,
       destination: '/home'
     }
-  }
+  },
+  vite: {
+    plugins: [
+      Icons({
+        compiler: 'astro',
+      }),
+    ],
+  },
 });
