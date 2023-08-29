27616e7d9bbe389f02608d040692e72d945c6053
Kyeonghwan Son <snapperbay4453@gmail.com>
2023-08-29 21:48:21 +0900

    apply Tailwind CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact(), tailwind()],
+  redirects: {
+    '/': {
+      status: 302,
+      destination: '/home'
+    }
+  }
 });
---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		theme: {
+			fontFamily: {
+				sans: ['"Nanum Gothic"', ...defaultTheme.fontFamily.sans],
+				monospace: ['"Nanum Gothic Coding"', ...defaultTheme.fontFamily.sans],
+			}
+		},
+		extend: {},
+	},
+	plugins: [],
+};
