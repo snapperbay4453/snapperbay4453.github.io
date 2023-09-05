7ea442dda0f5f907a6f5cf7fddd37a486e52cc04
Kyeonghwan Son <snapperbay4453@gmail.com>
2023-09-06 07:43:43 +0900

    fix typo about color of icons

--- src/composites/ThemeChangerInner.tsx
+++ src/composites/ThemeChangerInner.tsx
@@ -37,19 +37,19 @@ export default function ThemeChangerInner() {
             switch(theme) {
               case 'auto':
                 return (
-                  <i class='icon-[material-symbols--night-sight-auto-outline-rounded] bg-current'></i>
+                  <i class='icon-[material-symbols--night-sight-auto-outline-rounded] bg-zinc-100'></i>
                 );
               case 'light':
                 return (
-                  <i class='icon-[material-symbols--sunny-outline-rounded] bg-current'></i>
+                  <i class='icon-[material-symbols--sunny-outline-rounded] bg-zinc-100'></i>
                 );
               case 'dark':
                 return (
-                  <i class='icon-[material-symbols--mode-night-outline-rounded] bg-current'></i>
+                  <i class='icon-[material-symbols--mode-night-outline-rounded] bg-zinc-100'></i>
                 );
               default:
                 return (
-                  <i class='icon-[material-symbols--fiber-manual-record-outline] bg-current'></i>
+                  <i class='icon-[material-symbols--fiber-manual-record-outline] bg-zinc-100'></i>
                 );
             }
           })()}
