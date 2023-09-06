0d2637610b16ad6747ca68eda582d87e1294cf03
snapperbay4453 <snapperbay4453@users.noreply.github.com>
2023-09-06 11:54:55 +0000

    deploy: fcbe2ac0b5830d134e06e771a8301a6660e6eaca

---
+++ _astro/useClickOutside.4566ef30.js
@@ -0,0 +1,1 @@
+import{p as r}from"./hooks.module.6eb5c72e.js";const o=(e,n)=>{const t=c=>{e.current&&!e.current.contains(c.target)&&n()};r(()=>(document.addEventListener("click",t),()=>{document.removeEventListener("click",t)}))};export{o as u};
