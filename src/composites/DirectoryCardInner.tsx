a6ed3dd3d4d9c2b664886b2d8a9a79f532c336c4
Kyeonghwan Son <snapperbay4453@gmail.com>
2023-09-11 05:48:39 +0900

    adjust DirectoryCard component on mobile device width viewport

--- src/composites/DirectoryCardInner.tsx
+++ src/composites/DirectoryCardInner.tsx
@@ -15,19 +15,34 @@ export default function DirectoryCardInner({
 }: DirectoryCardInnerProps) {
   const renderCard = () => {
     return (
-      <div class='relative border-2 border-zinc-200 dark:border-zinc-800 rounded-3xl hover:bg-zinc-200 dark:hover:bg-zinc-800 h-40 w-60 max-w-[80vw] overflow-hidden'>
+      <div class={`
+        relative border-2 border-zinc-200 dark:border-zinc-800 rounded-3xl
+        hover:bg-zinc-200 dark:hover:bg-zinc-800
+        h-20 w-60 max-w-[80vw]
+        sm:h-40
+        overflow-hidden
+      `}>
         <img
-            class='absolute -bottom-4 -right-4 h-auto w-28 object-contain'
+            class={`
+              absolute
+              -bottom-1 -right-1 h-auto w-14
+              sm:-bottom-4 sm:-right-4 sm:h-auto sm:w-28
+              object-contain
+            `}
             src={imagePath}
         ></img>
-        <div class='absolute top-0 left-0 w-40 pl-4 pt-4'>
+        <div class={`
+          absolute
+          top-0 left-0 w-40 pl-4 pt-2
+          sm:top-0 sm:left-0 sm:w-40 sm:pl-4 sm:pt-4
+        `}>
           <span
-            class='block text-2xl font-bold'
+            class='block text-xl sm:text-2xl font-bold'
           >
             {title}
           </span>
           <span
-            class='block text-xl font-bold'
+            class='block text-lg sm:text-xl font-bold'
           >
             {description}
           </span>
