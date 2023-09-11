b21df136b22db4100a303e2205f56770f9451995
snapperbay4453 <snapperbay4453@users.noreply.github.com>
2023-09-11 11:30:53 +0000

    deploy: 2fa1aa123bc4b76e64b42ab24318033d1295e207

---
+++ _astro/FloatingActionButtonGroup.81ca7af3.js
@@ -0,0 +1,4 @@
+import{h as s,_ as a}from"./hooks.module.b1431384.js";import{u}from"./useClickOutside.088c6c5d.js";import{I as m}from"./IconButton.8947e91f.js";import{o}from"./jsxRuntime.module.1b1126d6.js";import"./preact.module.4d513d4a.js";function v({children:n}){const[t,e]=s(!1),c=()=>{e(r=>!r)},l=()=>{e(!1)},i=a();return u(i,l),o("div",{class:"relative flex flex-col-reverse gap-4 z-40",ref:i,children:[o("div",{class:`
+        absolute bottom-[calc(115%)] ${t?"opacity-100 ":"hidden"}
+        transition-opacity
+      `,children:n}),o(m,{onClick:c,children:o("i",{class:t?"icon-[material-symbols--close-rounded] text-zinc-100":"icon-[material-symbols--more-vert] text-zinc-100"})})]})}export{v as default};
