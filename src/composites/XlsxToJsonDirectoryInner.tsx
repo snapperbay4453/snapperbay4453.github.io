7a64ac123561fc3eb9f0824c2817654414139aff
Kyeonghwan Son <snapperbay4453@gmail.com>
2023-09-17 06:06:14 +0900

    update dependencies

--- src/composites/XlsxToJsonDirectoryInner.tsx
+++ src/composites/XlsxToJsonDirectoryInner.tsx
@@ -1,53 +1,97 @@
 import { useState, useRef } from 'preact/hooks';
-import { createTemplateI18nXlsx, convertI18XlsxToJsonDirectory } from 'i18n-xlsx-to-json-directory';
+import {
+  createTemplateI18nXlsx,
+  convertI18XlsxToJsonDirectoryZip,
+  convertJsonDirectoryZipToI18Xlsx
+} from 'i18n-xlsx-to-json-directory';
 import Button from '@/components/Button';
+import Card from '@/components/Card';
 
 export default function HeaderInner() {
-  const fileInputRef = useRef<HTMLInputElement>();
-  const [filename, setFilename] = useState<string>();
+  const xlsxFileInputRef = useRef<HTMLInputElement>();
+  const jsonZipFileInputRef = useRef<HTMLInputElement>();
+  const [xlsxFilename, setXlsxFilename] = useState<string>();
+  const [jsonZipFilename, setJsonZipFilename] = useState<string>();
 
   const downloadI18nJsonZip = () => {
     if(fileInputRef?.current) {
-      convertI18XlsxToJsonDirectory((fileInputRef.current.files ?? [])[0]);
+      convertI18XlsxToJsonDirectoryZip((fileInputRef.current.files ?? [])[0]);
+    }
+  };
+
+  const downloadI18nXlsx = () => {
+    if(jsonZipFileInputRef?.current) {
+      convertJsonDirectoryZipToI18Xlsx((jsonZipFileInputRef.current.files ?? [])[0]);
     }
   };
 
   return (
     <div class='flex flex-col items-center gap-4'>
-      <div class='flex items-center gap-4'>
+      <div class='mt-8'>
+        <div class='flex items-center gap-4'>
+          <Button
+            onClick={() => createTemplateI18nXlsx()}
+          >
+            .xlsx 템플릿 다운로드
+          </Button>
+        </div>
+      </div>
+      <div class='flex flex-col items-center gap-4 p-4 mt-8'>
+        <p class='text-2xl font-bold text-center'>XLSX → JSON</p>
         <Button
-          onClick={() => createTemplateI18nXlsx()}
-        >
-          .xlsx 템플릿 다운로드
-        </Button>
-        <Button
-          onClick={() => fileInputRef.current?.click()}
+          onClick={() => xlsxFileInputRef.current?.click()}
         >
           .xlsx 파일 선택
         </Button>
-      </div>
-      <br/>
-      <input
-        ref={fileInputRef}
-        class='sr-only'
-        type='file'
-        onchange={() => setFilename((fileInputRef.current?.files ?? [])[0]?.name)}
-      />
-      <div>
-        {filename && (
-          <div class='flex flex-col items-center gap-4'>
-            <span>선택된 파일: {filename}</span>
+        <input
+          ref={xlsxFileInputRef}
+          class='sr-only'
+          type='file'
+          onchange={() => setXlsxFilename((xlsxFileInputRef.current?.files ?? [])[0]?.name)}
+        />
+        {xlsxFilename && (
+          <>
+            <span>{xlsxFilename}</span>
             <Button
               onClick={() => downloadI18nJsonZip()}
             >
-              JSON 디렉토리 다운로드
+              변환
             </Button>
-          </div>
+          </>
         )}
-        {!filename && (
-          <div>
+        {!xlsxFilename && (
+          <>
             <span>{'선택된 파일이 없습니다.'}</span>
-          </div>
+          </>
+        )}
+      </div>
+      <div class='flex flex-col items-center gap-4 p-4 mt-8'>
+        <p class='text-2xl font-bold text-center'>JSON → XLSX</p>
+        <Button
+          onClick={() => jsonZipFileInputRef.current?.click()}
+        >
+          .zip 파일 선택
+        </Button>
+        <input
+          ref={jsonZipFileInputRef}
+          class='sr-only'
+          type='file'
+          onchange={() => setJsonZipFilename((jsonZipFileInputRef.current?.files ?? [])[0]?.name)}
+        />
+        {jsonZipFilename && (
+          <>
+            <span>{jsonZipFilename}</span>
+            <Button
+              onClick={() => downloadI18nXlsx()}
+            >
+              변환
+            </Button>
+          </>
+        )}
+        {!jsonZipFilename && (
+          <>
+            <span>{'선택된 파일이 없습니다.'}</span>
+          </>
         )}
       </div>
     </div>
