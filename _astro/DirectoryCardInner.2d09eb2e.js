142d96478d6ac9c369087465fd4b0f41ba7d6c04
snapperbay4453 <snapperbay4453@users.noreply.github.com>
2023-10-23 14:37:24 +0000

    deploy: f8fab44ed6e943bc5bad1ff294bdeac256961161

--- _astro/DirectoryCardInner.2d09eb2e.js
+++ _astro/DirectoryCardInner.2d09eb2e.js
@@ -1,4 +1,4 @@
-import{o as r}from"./jsxRuntime.module.5efcb3d3.js";import s from"./ImageFader.d010394b.js";import"./preact.module.ac6080a5.js";import"./hooks.module.c1775b87.js";function d({children:o,onClick:e}){const t=()=>r("div",{class:`
+import{o as r}from"./jsxRuntime.module.5efcb3d3.js";import s from"./ImageFader.a0d490cd.js";import"./preact.module.ac6080a5.js";import"./hooks.module.c1775b87.js";function d({children:o,onClick:e}){const t=()=>r("div",{class:`
           relative border-2 border-zinc-200 dark:border-zinc-800 rounded-3xl
           hover:bg-zinc-200 dark:hover:bg-zinc-800
           cursor-pointer
@@ -10,11 +10,11 @@ import{o as r}from"./jsxRuntime.module.5efcb3d3.js";import s from"./ImageFader.d
             block sm:hidden
             absolute
             -bottom-1 -right-1 h-auto w-14
-          `,children:r(s,{src:t??"",height:52})}),r("div",{class:`
+          `,children:r(s,{src:t??"",height:52,rounded:!1})}),r("div",{class:`
             hidden sm:block
             absolute
-            sm:-bottom-4 sm:-right-4 sm:h-auto sm:w-28
-          `,children:r(s,{src:t??"",height:104})}),r("div",{class:`
+            sm:-bottom-2 sm:-right-2 sm:h-auto sm:w-28
+          `,children:r(s,{src:t??"",height:104,rounded:!1})}),r("div",{class:`
           absolute
           top-0 left-0 w-40 pl-4 pt-2
           sm:top-0 sm:left-0 sm:w-40 sm:pl-4 sm:pt-4
